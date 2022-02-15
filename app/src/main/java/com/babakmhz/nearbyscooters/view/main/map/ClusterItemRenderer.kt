bb6bf9774582e344d4ac3e796349f8c5790befca
babakmhz <babakmhz96@gmail.com>
2022-02-15 15:14:40 +0330

    small modifaction on cluster renderer

--- app/src/main/java/com/babakmhz/nearbyscooters/view/main/map/ClusterItemRenderer.kt
+++ app/src/main/java/com/babakmhz/nearbyscooters/view/main/map/ClusterItemRenderer.kt
@@ -11,14 +11,15 @@ import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
 
 class ClusterItemRenderer(
-    private val context: Context,
-    private val googleMap: GoogleMap,
-    private val clusterManager: ClusterManager<Scooter>
+    context: Context,
+    googleMap: GoogleMap,
+    clusterManager: ClusterManager<Scooter>
 ) : DefaultClusterRenderer<Scooter>(context, googleMap, clusterManager) {
 
     override fun onBeforeClusterItemRendered(item: Scooter, markerOptions: MarkerOptions) {
         super.onBeforeClusterItemRendered(item, markerOptions)
         markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.electric_scooter))
+        markerOptions.title(item.title)
     }
 
     override fun onBeforeClusterRendered(cluster: Cluster<Scooter>, markerOptions: MarkerOptions) {
