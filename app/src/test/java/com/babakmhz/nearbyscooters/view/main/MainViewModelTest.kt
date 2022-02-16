8554019f29e4302b508dd7d162edd92cd0aeb8f5
babakmhz <babakmhz96@gmail.com>
2022-02-16 23:27:21 +0330

    mainViewModleTest.kt added

---
+++ app/src/test/java/com/babakmhz/nearbyscooters/view/main/MainViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.babakmhz.nearbyscooters.view.main
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MainViewModelTest{
+
+}
